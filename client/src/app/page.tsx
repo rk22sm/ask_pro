553f8afe219935ab0ce1428c34f24baff692275b
rk22sm <sm22.24ms@gmail.com>
2025-08-03 10:40:50 +0600

    feat: add home page contents

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,21 @@
+import CTA from "@/components/home/CTA";
+import Footer from "@/components/home/Footer";
+import Hero from "@/components/home/Hero";
+import Navbar from "@/components/home/Navbar";
+import Problems from "@/components/home/Problems";
+import Solutions from "@/components/home/Solutions";
+import Stats from "@/components/home/Stats";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Navbar />
+      <Hero />
+      <Problems />
+      <Solutions />
+      <Stats />
+      <CTA />
+      <Footer />
     </>
   );
 }
